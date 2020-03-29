72eb7a941f7dcfe844e082c7e63612f8987ad223
Jundi <izzuddinsalim@gmail.com>
2020-03-29 22:24:20 +0700

    menambahkan folder templates di dalam folder views

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,6 +6,8 @@ class Auth extends CI_Controller
 
     public function index()
     {
+        $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,59 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Page Title - SB Admin</title>
-
-    <link href="<?= base_url('assets/'); ?>css/styles.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
-</head>
-
-<body class="bg-primary">
-    <div id="layoutAuthentication">
-        <div id="layoutAuthentication_content">
-            <main>
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-5 mt-5">
-                            <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">Login</h3>
-                                </div>
-                                <div class="card-body">
-                                    <form>
-                                        <div class="form-group">
-                                            <input class="form-control py-4" id="username" name="username" type="text" placeholder="Enter username..." />
-                                        </div>
-                                        <div class="form-group">
-                                            <input class="form-control py-4" id="password" name="password" type="password" placeholder="Enter password" />
-                                        </div>
-                                        <div class="form-group d-flex align-items-center justify-content-between mt-5 mb-0">
-                                            <a class="small" href="password.html">Forgot Password?</a>
-                                            <button type="submit" class="btn btn-primary" href="index.html">Login</but>
-                                        </div>
-                                    </form>
-                                </div>
-                                <div class="card-footer text-center">
-                                    <div class="small">
-                                        <a href="register.html">Need an account? Sign up!</a>
+<div id="layoutAuthentication">
+    <div id="layoutAuthentication_content">
+        <main>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 mt-5">
+                        <div class="card shadow-lg border-0 rounded-lg mt-5">
+                            <div class="card-header">
+                                <h3 class="text-center font-weight-light my-4">Login</h3>
+                            </div>
+                            <div class="card-body">
+                                <form>
+                                    <div class="form-group">
+                                        <input class="form-control py-4" id="username" name="username" type="text" placeholder="Enter username..." />
                                     </div>
+                                    <div class="form-group">
+                                        <input class="form-control py-4" id="password" name="password" type="password" placeholder="Enter password" />
+                                    </div>
+                                    <div class="form-group d-flex align-items-center justify-content-between mt-5 mb-0">
+                                        <a class="small" href="password.html">Forgot Password?</a>
+                                        <button type="submit" class="btn btn-primary" href="index.html">Login</but>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="card-footer text-center">
+                                <div class="small">
+                                    <a href="register.html">Need an account? Sign up!</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </main>
-        </div>
-
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="<?= base_url('assets/'); ?>js/scripts.js"> </script>
-
-</body>
-
-</html>
+            </div>
+        </main>
+    </div>
