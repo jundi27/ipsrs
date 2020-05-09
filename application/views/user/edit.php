371ed06e7a8fccba56539195cd5ffab1fc0f2b8e
Jundi <izzuddinsalim@gmail.com>
2020-05-09 21:39:31 +0700

    memperbaiki views yang bentrok

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -67,21 +67,21 @@
 
             <?= form_open_multipart('user/edit'); ?>
             <div class="form-group row">
-                <label for="email" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
+                <label for="email" class="col-sm-3 col-form-label">Email</label>
+                <div class="col-sm-9">
                     <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
                 </div>
             </div>
             <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">Nama Lengkap</label>
-                <div class="col-sm-10">
+                <label for="name" class="col-sm-3 col-form-label">Nama Lengkap</label>
+                <div class="col-sm-9">
                     <input type="text" class="form-control" id="nama" name="nama" value="<?= $user['nama']; ?>">
                     <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
             </div>
             <div class="form-group row">
-                <div class="col-sm-2">Gambar</div>
-                <div class="col-sm-10">
+                <div class="col-sm-3">Gambar</div>
+                <div class="col-sm-9">
                     <div class="row">
                         <div class="col-sm-3">
                             <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail">
@@ -97,7 +97,7 @@
             </div>
 
             <div class="form-group row justify-content-end">
-                <div class="col-sm-10">
+                <div class="col-sm-9">
                     <button type="submit" class="btn btn-primary">Edit</button>
                 </div>
             </div>
