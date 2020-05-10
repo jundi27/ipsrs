d331a09829b04633f31fa471dcc3cd56dc0311ea
Ibnu Afdhal <ibnuafdhal10@gmail.com>
2020-05-10 15:54:10 +0700

    Membuat file kelolaalkes dan kelola teknisi, mengedit baris di sidebar admin, mengedit baris di sidebar teknisi

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -68,7 +68,7 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="<?= base_url('admin/pemeliharaan'); ?>" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                <i class="fas fa-fw fa-folder"></i>
+                <i class="fas fa-fw fa-briefcase-medical"></i>
                 <span>Pemeliharaan</span>
             </a>
 
@@ -82,6 +82,21 @@
 
         <hr class="sidebar-divider d-none d-md-block">
 
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('admin/kelolaalkes'); ?>">
+                <i class="fas fa-fw fa-laptop-medical"></i>
+                <span>Kelola Data Alat Kesehatan</span>
+            </a>
+        </li>
+
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('admin/kelolateknisi'); ?>">
+                <i class="fas fa-fw fa-users-cog"></i>
+                <span>Kelola Data Teknisi</span>
+            </a>
+        </li>
 
 
         <!-- Nav Item -->
